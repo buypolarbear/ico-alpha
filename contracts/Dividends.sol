b338a9c9ebdf75d16a77908a115937cf7e392d02
Alessandro Marin <alessandro.m316@gmail.com>
2017-12-09 23:39:25 +0800

    calculates dividends

---
+++ contracts/Dividends.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+contract Dividends is StandardToken {
+  address owner;
+
+  // expose these for ERC20 tools
+  string public name = "TODO";
+  string public symbol = "TODO";
+  uint public decimals = 18;
+
+
+  uint public tokensIssued = 10000 ether;
+  uint public tokensFrozen = holdings * 3;
+
+  function Dividends() {
+    owner = msg.sender;
+  }
+
+  function calculateDividends(uint256 profit) {
+    // profit in finney
+    uint256 totalDividends = tokensIssued * (profit / 2);
+    uint256 airdrop = totalDividends / ((tokensIssued + totalDividends) / totalDividends);
+  }
+}
