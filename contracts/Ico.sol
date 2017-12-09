5d771a6401462f112d91d782ef41665b205500b5
Mike van Rossum <mike@mikevanrossum.nl>
2017-12-09 21:50:44 +0800

    use truffle

--- contracts/Ico.sol
+++ contracts/Ico.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import 'node_modules/zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
 contract Fund is StandardToken {
   address owner;
