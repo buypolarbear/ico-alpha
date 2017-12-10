23747f6d7fbd847c6deb288f24e77c23bd31e275
Alessandro Marin <alessandro.m316@gmail.com>
2017-12-10 17:15:19 +0800

    random dividends stuff

--- contracts/Dividends.sol
+++ contracts/Dividends.sol
@@ -1,27 +1,30 @@
 pragma solidity ^0.4.17;
 
-import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/token/BasicToken.sol';
 
-contract Dividends is StandardToken {
+contract Dividends is BasicToken {
   address owner;
 
   // expose these for ERC20 tools
   string public name = "TODO";
   string public symbol = "TODO";
-  uint public decimals = 18;
+  uint256 public decimals = 18;
 
 
-  uint public tokensIssued = 10000 ether;
-  uint public tokensFrozen = tokensIssued * 3;
+  uint256 public pointsMultiplier = 10e18;
+  uint256 public tokensIssued = 3000;
+  uint256 public AUM = 6000;
+  uint256 public tokensFrozen = tokensIssued * 3;
 
   function Dividends() public {
     owner = msg.sender;
   }
 
   function calculate(uint256 profit) public returns (uint256) {
-    // profit in finney
-    uint256 totalDividends = tokensIssued * (profit / 2);
-    uint256 airdrop = totalDividends / ((tokensIssued + totalDividends) / totalDividends);
-    return airdrop * 3;
+    // profit in USD
+    uint256 newAUM = AUM + profit;
+    uint256 newTokenValue = (newAUM * 10e10) / (tokensIssued * 10e2); // 8 sig digits
+    uint256 dividends = (profit * 10e16) / newTokenValue; // 8 sig digits
+    return dividends;
   }
 }
