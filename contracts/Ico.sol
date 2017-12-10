6b0ccd673cc9d0e23ce039723964ce84a542c132
Mike van Rossum <mike@mikevanrossum.nl>
2017-12-10 13:56:32 +0800

    add dividend helpers and monkey path transfer and balanceOf

--- contracts/Ico.sol
+++ contracts/Ico.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.17;
 
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
-contract Ico is StandardToken {
+// NOTE: BasicToken only has partial ERC20 support
+contract Ico is BasicToken {
 
   address owner;
 
@@ -16,14 +17,40 @@ contract Ico is StandardToken {
   function Ico() {
     owner = msg.sender;
 
-    uint mikesPremine = 15 ether;
+    uint mikesPremine = 16 ether;
 
     totalSupply = SUPPLY;
     balances[owner] = SUPPLY - mikesPremine;
-    balances[0x0d1d4e623D10F9FBA5Db95830F7d3839406C6AF2] = mikesPremine;
+    balances[0x7412417A91af476C48516283B3CfA80BA6E489A8] = mikesPremine;
   }
 
   function sayHi() pure returns (string)  {
-    return "hello";
+    return "hello 3";
   }
+
+
+  // getter to retrieve divident owed
+  function getOwedDividend(address _owner) public view returns (uint256 balance) {
+    // todo @ale
+    return 1 ether;
+  }
+
+  // helper function that makes sure we add dividend before any
+  // type of ledger mutation.
+  modifier addDividend() {
+    uint owedDividend = getOwedDividend(msg.sender);
+    if(owedDividend > 0) {
+      balances[msg.sender] = balances[msg.sender].add(owedDividend);
+    }
+    _;
+  }
+
+  // monkey patches
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return BasicToken.balanceOf(_owner) + getOwedDividend(_owner);
+  }
+  function transfer(address _to, uint256 _value) addDividend() public returns (bool) {
+    return BasicToken.transfer(_to, _value);
+  }
+
 }
