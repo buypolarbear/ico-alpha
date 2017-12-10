37bf22ba343c4e014e06e59ceec34b40b197a617
Evelyn-McDonald <evel.mcdonald@gmail.com>
2017-12-10 12:19:26 +0800

    creates skeleton for ICO functions

--- contracts/Ico.sol
+++ contracts/Ico.sol
@@ -4,19 +4,94 @@ import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
 contract Fund is StandardToken {
   address owner;
+  address[] team;
 
   // expose these for ERC20 tools
   string public name = "TODO";
   string public symbol = "TODO";
-  uint public decmials = 18;
+  uint public decimals = 18;
 
+  uint public constant HARD_CAP = 10000;
+  
+  uint public tokensIssued;
+  uint public tokensFrozen;
+  
+  uint tokensPerETH;
 
-  uint public SUPPLY = 10000;
+  uint public tokenSaleDeadline;
 
+  /**
+   *  Fund constructor
+   */
   function Fund() {
     owner = msg.sender;
+  }
 
-    totalSupply = SUPPLY;
-    balances[owner] = SUPPLY;
+
+  /**
+   * Modifiers
+   */
+  modifier onlyOwner() {
+    require (msg.sender == owner);
+    _;
+  }
+
+  modifier duringICO() {
+    require (block.number <= tokenSaleDeadline);
+    _;
+  }
+
+  modifier duringICO() {
+    require (block.number > tokenSaleDeadline);
+    _;
+  }
+
+
+  /**
+   * 
+   */
+  function create(uint _tokensPerETH, address[] _team) onlyOwner returns (bool) {
+    tokensPerETH = _tokensPerETH;
+    team = _team;
+
+    tokensIssued = 0;
+    tokensFrozen = 0;
+    
+    tokenSaleDeadline = block.number + 1000;
+
+    return true;
+  }
+
+  /**
+   * Function allowing investors to participate in the ICO. 
+   * Fund tokens will be distributed based on amount of ETH sent by investor, and calculated
+   * using tokensPerETH value.
+   */
+  function participate() public payable duringICO returns (bool) {
+    uint amountReceived = msg.value;
+    uint tokensToReturn = amountReceived * tokensPerETH;t
+
+    if (tokensToReturn + tokensIssued > HARD_CAP) throw;
+
+    // update investor balance
+    tokensIssued += tokensToReturn;
+    tokensFrozen = tokensIssued * 2;
+
+    return true;
+  }
+
+  /**
+   * Withdraw ICO funds from smart contract.
+   */
+  function withdraw() public onlyOwner returns (bool) afterICO {
+    owner.transfer(this.balance);
+    return true;
+  }
+
+  /**
+   * Withdraw all funds and kill fund smart contract
+   */
+  function liquidate() returns (bool) {
+    return true;
   }
 }
