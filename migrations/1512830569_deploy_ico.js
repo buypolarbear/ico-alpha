2581152ca36e9e3861568427c5ea859f9adbb47e
Mike van Rossum <mike@mikevanrossum.nl>
2017-12-10 19:56:20 +0800

    update migration to pass data to contract contructor

--- migrations/1512830569_deploy_ico.js
+++ migrations/1512830569_deploy_ico.js
@@ -1,5 +1,16 @@
+const moment = require('moment');
 const Ico = artifacts.require('Ico')
 
 module.exports = function(deployer) {
-  deployer.deploy(Ico)
+
+  const start = moment().add(1, 'minute').unix();
+  const end = moment().add(10, 'minute').unix();
+  const team = [
+    '0x627306090abab3a6e1400e9345bc60c78a8bef57',
+    '0xf17f52151ebef6c7334fad080c5704d77216b732'
+  ];
+
+  const tokensPerEth = 400;
+
+  deployer.deploy(Ico, start, end, team, tokensPerEth);
 }
