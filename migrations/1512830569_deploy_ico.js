349e97a4d4a850b62405272152bbb028f566ace3
Mike van Rossum <mike@mikevanrossum.nl>
2017-12-10 20:02:11 +0800

    add comment

--- migrations/1512830569_deploy_ico.js
+++ migrations/1512830569_deploy_ico.js
@@ -5,11 +5,15 @@ module.exports = function(deployer) {
 
   const start = moment().add(1, 'minute').unix();
   const end = moment().add(10, 'minute').unix();
+
+  // first two accounts of mnemonic:
+  // `candy maple cake sugar pudding cream honey rich smooth crumble sweet treat`
   const team = [
     '0x627306090abab3a6e1400e9345bc60c78a8bef57',
     '0xf17f52151ebef6c7334fad080c5704d77216b732'
   ];
 
+  // this should be USD/ETH rate
   const tokensPerEth = 400;
 
   deployer.deploy(Ico, start, end, team, tokensPerEth);
