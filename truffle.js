a51231f18bdad520572a3af61ff121f8e2cbf297
Alessandro Marin <alessandro.m316@gmail.com>
2017-12-18 15:54:33 +0800

    adds tokens issued on aum on participate

--- truffle.js
+++ truffle.js
@@ -10,6 +10,7 @@ module.exports = {
       port: 8545,
       network_id: "*",
       from: "0x0034C0b4428081508A8E77B0d31da9Bf73Bb95a7",
+      // from: "0xd6bB00fdf0557D07782cA55778a4f7DF07a77Dd7",
       gas: 4700036
     }
   }
