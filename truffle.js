26e7dc0efdffb3462e219e5d76351c153072365d
Alessandro Marin <alessandro.m316@gmail.com>
2017-12-18 14:12:13 +0800

    readds ropsten network

--- truffle.js
+++ truffle.js
@@ -4,6 +4,12 @@ module.exports = {
       host: "localhost",
       port: 9545,
       network_id: "*" // Match any network id
+    },
+    ropsten {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      from: "0x0034C0b4428081508A8E77B0d31da9Bf73Bb95a7"
     }
   }
 };
