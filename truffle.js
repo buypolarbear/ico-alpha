fd96c3137943e101284f16639c90fe72a8f691d8
Alessandro Marin <alessandro.m316@gmail.com>
2017-12-19 12:35:13 +0800

    adds network for mainnet

--- truffle.js
+++ truffle.js
@@ -12,6 +12,13 @@ module.exports = {
       from: "0x0034C0b4428081508A8E77B0d31da9Bf73Bb95a7",
       // from: "0xd6bB00fdf0557D07782cA55778a4f7DF07a77Dd7",
       gas: 4700036
+    },
+    mainnet: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      from: "0xd679CCa0487cf876d19393baf70bBD68Ad3831e6", // Ploutos Owner Account
+      gas: 4700036
     }
   }
 };
